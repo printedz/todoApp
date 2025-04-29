2e1a397468e7435edf53a34ab8a3d1e9de5eaac7
alejandro <alejandrorrnn@icloud.com>
2025-04-29 19:47:01 +0200

    Initial Commit

---
+++ todo-list-3/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  todo-list-3
+//
+//  Created by Alejandro Regidor Nova on 29/4/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ todo-list-3/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  todo-list-3
+//
+//  Created by Alejandro Regidor Nova on 29/4/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
